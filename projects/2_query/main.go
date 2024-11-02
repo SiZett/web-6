d2568b0aa66c0125b37f317e8380d1ee9ca25d6b
SiZett <oleg.elnich@yandex.ru>
2024-11-02 10:01:25 +0300

    lab6

--- projects/2_query/main.go
+++ projects/2_query/main.go
@@ -1,9 +1,50 @@
 package main
 
-// здесь надо написать код
+// некоторые импорты нужны для проверки
+import (
+	"fmt"
+	"net/http"
+	"strconv" // вдруг понадобиться вам ;)
+)
 
-func main() {
-	// и здесь тоже
+var count1 int = 0
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(strconv.Itoa(count1)))
+		return
+	} else if r.Method == "POST" {
+		r.ParseForm()
+		s := r.FormValue("count")
+		if s == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("это не число"))
+
+			return
+		}
+		number, err := strconv.Atoi(s)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("это не число"))
+
+			return
+		}
+		count1 += number
+		return
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Метод не поддерживается"))
+		return
+	}
+
 }
 
-// и тут тоже (если очень надо)
+func main() {
+	http.HandleFunc("/count", handler)
+
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера!")
+	}
+}
